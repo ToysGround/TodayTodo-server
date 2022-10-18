8856da481add50956afab1bd3a7b77fc4dac0db5
byunjin <byon1228@gmail.com>
2022-10-18 21:05:09 +0900

    first commit

---
+++ src/main/java/com/toysground/todaytodo/TodayTodoApplication.java
@@ -0,0 +1,13 @@
+package com.toysground.todaytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toysground/todaytodo/TodayTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toysground.todaytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
